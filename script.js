890127201eb259e5c1fbe06c232a437b6658c89a
Ishan <ishanutsav.rnc1234@gmail.com>
2024-03-28 20:43:37 +0530

    ishan

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function moveRandomEl(elm) {
     elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
   }
   
-  const moveRandom = document.querySelector("#move-random");
+  const moveRandom = no1.querySelector("#move-random");
   
   moveRandom.addEventListener("mouseenter", function (e) {
     moveRandomEl(e.target);
